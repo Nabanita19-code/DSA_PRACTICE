8861a828d125c6ce5b9d316eaa3697e1083dbe1b
Nabanita Mukherjee <nabanitamukherjee121@gmail.com>
2025-07-15 19:59:55 +0530

    9. Palindrome Number

---
+++ PalindromeNumber.c
@@ -0,0 +1,13 @@
+#include<stdbool.h>
+bool isPalindrome(int x) {
+    if (x < 0) return false;
+    int original = x;
+    long N = 0;
+    while (x !=0) {
+        int digit = x%10;
+        N = N*10 + digit;
+        x/=10;
+    }
+        return original == N;
+    
+}
